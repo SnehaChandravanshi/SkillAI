c31190f6b28327355c7c6e69f4e1ccb31c464b48
Sneha Chandravanshi <161471021+SnehaChandravanshi@users.noreply.github.com>
2026-02-19 17:56:55 +0530

    Fix route wildcard pattern for serving index.html

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use("/api/profile", require("./routes/profile.routes"));
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 const PORT = process.env.PORT || 5000;
